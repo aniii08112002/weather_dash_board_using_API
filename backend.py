568d1ffe9ae7640acc0f58b68881e3c13d177b72
audit <anirudhsharmakr76@gmail.com>
2023-08-29 22:15:22 +0530

    filtering

--- backend.py
+++ backend.py
@@ -7,7 +7,14 @@ def get_data(place,forcast_days=None,kind=None):
     response=requests.get(url)
 
     data = response.json()
-    return data
+    filtered_data=data['list']
+    nr_values=8*forcast_days
+    filtered_data=filtered_data[:nr_values]
+    if kind=="Temperature":
+        filtered_data=[dict['main']['temp'] for dict in filtered_data]
+    if kind=="Sky":
+        filtered_data=[dict['weather'][0]['main'] for dict in filtered_data]
+    return filtered_data
 
 if __name__=="__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo",forcast_days=3,kind="Temperature"))
