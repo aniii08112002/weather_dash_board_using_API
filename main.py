5f377b263a1610fc5066e7be5d19762f77f0c7d3
audit <anirudhsharmakr76@gmail.com>
2023-09-12 06:14:12 +0530

    final projection

--- main.py
+++ main.py
@@ -7,11 +7,21 @@ days=st.slider("forecast days",min_value=1,max_value=5,
                help="select the number of forecast days")
 option=st.selectbox("Select data to view",("Temperature","Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
-data=get_data(place,days,option)
 
-    temperature=[days*i for i in temperature]
-    return date,temperature
-d,t=get_data(days)
+if place:
+    filtered_data=get_data(place,days)
 
-figure=px.line(x=d,y=t,labels={"x":"DATE","y":"TEMPERATURE (C)"})
-st.plotly_chart(figure)
+
+    if option=="Temperature":
+        temperatures=[dict['main']['temp'] for dict in filtered_data]
+        dates=[dict["dt_txt"] for dict in filtered_data]
+        figure=px.line(x=dates,y=temperatures,labels={"x":"DATE","y":"TEMPERATURE (C)"})
+        st.plotly_chart(figure)
+    if option=="Sky":
+        sky_conditions=[dict["weather"][0]["main"] for dict in filtered_data]
+        images={"Clear":"images/clear.png","Snow":"images/snow.png","Clouds":"images/cloud.png","Rain":"images/rain.png"}
+        image_path=[images[conditions] for conditions in sky_conditions]
+        print(sky_conditions)
+
+        st.image(image_path,width=115)
+
